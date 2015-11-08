c8930b6e4ac5ee94114f13675b79c061d4a5b02b
Ana Marjanovic <anamyers03@gmail.com>
2015-11-07 22:34:05 -0800

    11/7
    player and canvas

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+//**************ENEMY***********************************
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+// Enemies our player must avoid
+var Enemy = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -21,15 +21,38 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+//**************PLAYER***********************************
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+
+
+
+//**************FUNCTIONALITY***********************************
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = [];
+var player = new Player(202.5, 383, 50);
 
 
 // This listens for key presses and sends the keys to your
